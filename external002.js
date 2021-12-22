ad1dc9117a7f92ca678c6c6920b751ae8ecf13a8
manoj-choudhary <46576017+manoj-choudhary@users.noreply.github.com>
2021-12-23 00:12:49 +0530

    Update and rename external02.js to external002.js

--- external002.js
+++ external002.js
@@ -2,3 +2,6 @@ function doSomething(myelement, color, txtInput) {
 		myelement.style.color=color;
 		myelement.innerHTML = txtInput
 }
+function secondFunction(myelement, color, txtInput) {
+		alert("SecondMethsd")
+}
